f55ae008156183bd12e0ad086b8c3aee8ed1c52d
William Huang <wwhuang@umich.edu>
2013-07-14 02:13:09 -0400

    slowly fixing errors

--- apps/OpoBase/web_export.py
+++ apps/OpoBase/web_export.py
@@ -2,7 +2,7 @@ import sys
 import urllib2
 import json
 
-packet = {'g_id': 2}
+packet = {'G_ID': 2}
 
 while True:
     line = sys.stdin.readline()
@@ -10,17 +10,24 @@ while True:
         s = line.split(":")
         if "ID" in line:
             packet[s[0]] = int(s[1].strip(), 16)
-            print packet
         else:
             packet[s[0]] = int(s[1].strip())
-        if "Range" in line:
+        if "RANGE" in line:
             r = float(s[1].strip()) / 1000000.0
             packet[s[0]] = r
-        if "Hour" in line:
-            data = json.dumps(packet)
-            d_len = len(data)
-            url = 'http://fusion.eecs.umich.edu/raw_update'
-            req = urllib2.Request(url, data, {'Content-Type': 'application/json',
-                                              'Content-Length': d_len})
-            r = urllib2.urlopen(req)
-            print str(r.read())
+        if "TIME" in line:
+            print "Woo"
+            if len(packet) == 8:
+                print packet
+                data = json.dumps(packet)
+                d_len = len(data)
+                url = 'http://fusion.eecs.umich/raw_update'
+                req = urllib2.Request(url, data, {'Content-Type': 'application/json',
+                                                  'Content-Length': d_len})
+                r = urllib2.urlopen(req)
+                print str(r.read())
+            else:
+                print "Misformed Packet"
+                print str(packet)
+                print "--------------------"
+            packet = {'G_ID': 2}
