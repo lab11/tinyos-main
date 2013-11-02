88b548d249c2d08a2bda0588da78ac633ec2cb94
William Huang <wwhuang@umich.edu>
2013-11-02 01:09:19 -0400

    indent fixing

--- apps/OpoBase/visual_demo_write_to_csv.py
+++ apps/OpoBase/visual_demo_write_to_csv.py
@@ -4,7 +4,7 @@ import sys
 writer = csv.writer(open('visual.csv', 'wb'), delimiter=' ')
 
 while True:
-    line = sys.stdin.readline()
-    	s = line.strip()
-    	s = s.split()
-    	writer.writerow(s)
+	line = sys.stdin.readline()
+	s = line.strip()
+	s = s.split()
+	writer.writerow(s)
