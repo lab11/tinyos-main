425ddaa2f0d843ddc455952ac2c6b8ea434fbc20
William Huang <wwhuang@umich.edu>
2013-11-02 01:06:24 -0400

    file to write visual demo shit to csv file

---
+++ apps/OpoBase/visual_demo_write_to_csv.py
@@ -0,0 +1,10 @@
+import csv
+import sys
+
+writer = csv.writer(open('visual.csv', 'wb'), delimiter=' ')
+
+while True:
+    line = sys.stdin.readline()
+    	s = line.strip()
+    	s = s.split()
+    	writer.writerow(s)
