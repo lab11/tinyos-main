facce98416f43761ccffe57728223d3fc98ee8f0
William Huang <wwhuang@umich.edu>
2013-12-05 14:36:56 -0500

    moar fatures

--- apps/OpoBase/visual_demo_write_to_csv.py
+++ apps/OpoBase/visual_demo_write_to_csv.py
@@ -5,6 +5,23 @@ import time
 m_file = open('visual.csv', 'wb')
 writer = csv.writer(m_file, delimiter=' ')
 throwaway = sys.stdin.readline()
+receivers = {}
+transmitters = {}
+nodes = 6
+
+while True:
+	line = sys.stdin.readline()
+	s = line.strip().split()
+	if len(s) == 10:
+		receivers[s[1]] = 0
+		transmitters[s[2]] = 0
+
+	if len(receivers) == nodes and len (transmitters) == nodes:
+		print "Ready!"
+		break
+	else:
+		print "rx: " + str(len(receivers)) + " tx: " + str(len(transmitters))
+
 
 while True:
 	line = sys.stdin.readline()
@@ -14,3 +31,4 @@ while True:
 	s.append(current_time)
 	writer.writerow(s)
 	m_file.flush()
+	print s
